a260b7b300bd05dfc7d10279279cccc99904fdc3
Abu Raihan <raihan2bd.official@gmail.com>
2023-05-17 09:58:37 +0600

    Set the classroom for the student

--- student.rb
+++ student.rb
@@ -2,9 +2,12 @@ require_relative 'person'
 
 # The Student class represents a student who inherits from the Person class.
 class Student < Person
+  attr_accessor :classroom # Getter and Setter method for the classroom
+
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
+    @classroom.add_student(self) # Set the classroom for the student
   end
 
   # The play_hooky method returns a string representation of a student playing hooky.
