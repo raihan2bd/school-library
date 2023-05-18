084e0694dae9fda62a2469f5eaa9b2db6cb1c45f
Abu Raihan <raihan2bd.official@gmail.com>
2023-05-18 14:27:43 +0600

    Fix Student class to set the classroom for the student

--- student.rb
+++ student.rb
@@ -6,8 +6,7 @@ class Student < Person
 
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
-    @classroom = classroom
-    @classroom.add_student(self) # Set the classroom for the student
+    @classroom = classroom # Set the classroom for the student
   end
 
   # The play_hooky method returns a string representation of a student playing hooky.
