a8ab173db3fddb610b32361f3c8078a7dea16804
Abu Raihan <raihan2bd.official@gmail.com>
2023-05-24 15:29:26 +0600

    Add test for the Student class and it's methods

--- spec/helper_spec.rb
+++ spec/helper_spec.rb
@@ -1,3 +1,4 @@
 require_relative '../person'
 require_relative '../book'
 require_relative '../rental'
+require_relative '../student'
---
+++ spec/student_spec.rb
@@ -0,0 +1,44 @@
+require_relative './helper_spec'
+
+describe Student do
+  before :each do
+    @student = Student.new 'Class 12', 18, 'Lorem', parent_permission: true
+  end
+
+  describe '#new' do
+    it 'takes two patameters and returns a Book object' do
+      expect(@student).to be_an_instance_of Student
+    end
+  end
+
+  describe '#classroom' do
+    it 'returns the correct classromm of the student' do
+      expect(@student.classroom).to eql 'Class 12'
+    end
+  end
+
+  describe '#age' do
+    it 'returns the correct age of the student' do
+      expect(@student.age).to eql 18
+    end
+  end
+
+  describe '#name' do
+    it 'returns the correct name of the student' do
+      expect(@student.name).to eql 'Lorem'
+    end
+  end
+
+  describe '#parent_permission' do
+    it 'returns the correct parent_permission of the student' do
+      expect(@student.parent_permission).to eql true
+    end
+  end
+
+
+  describe 'play_hooky' do
+    it 'call the play_hooky method and return string' do
+      expect(@student.play_hooky).to eq('¯\(ツ)/¯')
+    end
+  end
+end
